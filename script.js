482395cbabec6167d81faa2cef11c11fc93595a6
Samiun Jahan <samiunjahan06@gmail.com>
2021-02-08 09:11:49 +0600

    third commit

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ const displaySongs = songs => {
             <div class="col-md-9">
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
-                <audio controls src="${song.preview}"><audio>
+                <audio controls>
+                <source src="${song.preview}" type="audio/ogg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
                <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
